1a683677a8ec9fa0c1a56d4db9a73ba76c350f3a
ShinEun9 <ses2201@naver.com>
2023-09-26 18:15:43 +0900

    feat: 라우팅 설정
    - react-router-dom, @types/react-router-dom 패지키 install
    - 페이지 별로 컴포넌트 생성
    - routing 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
 function App() {
-  return <div className="App"></div>;
+  const user = null;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={user ? <Home /> : <Navigate replace={true} to="/login" />}></Route>
+          <Route path="/login" element={!user ? <Login /> : <Navigate to="/" replace={true} />}></Route>
+          <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" replace={true} />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
