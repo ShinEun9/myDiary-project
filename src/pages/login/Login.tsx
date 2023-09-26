82d67e753740d50b342ec5722fe39278c06b3c23
ShinEun9 <ses2201@naver.com>
2023-09-26 21:37:21 +0900

    design: Login page UI 구현

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import styles from './Login.module.css';
+import { Link } from 'react-router-dom';
+import LoginHeader from '../../components/login/LoginHeader';
+import LoginForm from '../../components/login/LoginForm';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <>
+        <div className={styles['login-wrapper']}>
+          <LoginHeader />
+          <main>
+            <LoginForm />
+          </main>
+          <Link to="/signup" className={styles['btn-link']}>
+            회원가입하러가기
+          </Link>
+        </div>
+      </>
+    </>
+  );
 };
 
 export default Login;
