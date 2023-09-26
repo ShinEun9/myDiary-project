0d93736452b47b9c3097b31b9459a6c0c3b257d1
ShinEun9 <ses2201@naver.com>
2023-09-26 21:26:59 +0900

    remove: App.css 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import './App.css';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
@@ -8,15 +7,13 @@ import Signup from './pages/signup/Signup';
 function App() {
   const user = null;
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={user ? <Home /> : <Navigate replace={true} to="/login" />}></Route>
-          <Route path="/login" element={!user ? <Login /> : <Navigate to="/" replace={true} />}></Route>
-          <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" replace={true} />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={user ? <Home /> : <Navigate replace={true} to="/login" />}></Route>
+        <Route path="/login" element={!user ? <Login /> : <Navigate to="/" replace={true} />}></Route>
+        <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" replace={true} />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
