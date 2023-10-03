db44b69a0e45047e0bc26b0b21084ed2443cffc1
ShinEun9 <ses2201@naver.com>
2023-10-04 02:53:09 +0900

    feat: useLogin 훅 구현함으로써 로그인 기능 구현

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,25 +1,41 @@
+import { useState, ChangeEvent, FormEvent } from 'react';
+import { useLogin } from '../../hooks/useLogin';
 import Button from '../../components/common/Button';
 import Input from '../../components/common/Input';
 
 const LoginForm = () => {
-  const isPending = false;
-  const error = null;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { error, isPending, login, emailRef } = useLogin();
+
+  const handleData = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.type === 'email') {
+      setEmail(e.target.value);
+    } else if (e.target.type === 'password') {
+      setPassword(e.target.value);
+    }
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    login(email, password);
+  };
 
   return (
-    <form className={'form-wrap'}>
+    <form className={'form-wrap'} onSubmit={handleSubmit}>
       <label className={'label'} htmlFor="user-email">
         이메일
       </label>
-      <Input type="email" id="user-email" />
+      <Input ref={emailRef} type="email" id="user-email" onChange={handleData} />
 
       <label className={'label'} htmlFor="user-password">
         비밀번호
       </label>
-      <Input type="password" id="user-password" />
+      <Input type="password" id="user-password" onChange={handleData} />
 
       {!isPending && <Button>로그인</Button>}
-      {isPending && <strong>로그인이 진행중입니다...</strong>}
-      {error && <strong>{error}</strong>}
+      {isPending && <strong className="pending">로그인이 진행중입니다...</strong>}
+      {error && <strong className="error">* {error}</strong>}
     </form>
   );
 };
---
+++ src/hooks/useLogin.tsx
@@ -0,0 +1,34 @@
+import { useState, useRef } from 'react';
+import { appAuth } from '../firebase/config';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useAuthContext } from './useAuthContext';
+
+export const useLogin = () => {
+  const [error, setError] = useState<string | null>(null);
+  const [isPending, setIsPending] = useState(false);
+  const emailRef = useRef<HTMLInputElement>(null);
+
+  const { dispatch } = useAuthContext();
+
+  const login = (email: string, password: string) => {
+    setError(null);
+    setIsPending(true);
+
+    signInWithEmailAndPassword(appAuth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        dispatch({ type: 'login', payload: user });
+        setError(null);
+        setIsPending(false);
+      })
+      .catch((err) => {
+        setError('올바르지 않은 이메일 또는 비밀번호입니다.');
+        setIsPending(false);
+
+        emailRef.current?.focus();
+      });
+  };
+
+  return { error, isPending, login, emailRef };
+};
