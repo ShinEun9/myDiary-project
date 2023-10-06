f4a03bfb2f2d1923d61977f73b2d3a90c132c889
ShinEun9 <ses2201@naver.com>
2023-10-06 12:43:51 +0900

    feat: useFireStore훅 만
    - 훅 내부 (일기)업로드, 삭제, 수정 함수 구현
    - 업로드, 삭제, 수정함수 구현 시 useReducer훅 사용

---
+++ src/hooks/useFireStore.tsx
@@ -0,0 +1,97 @@
+import { useReducer } from 'react';
+import { appFireStore, timeStamp } from '../firebase/config';
+import { collection, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore';
+
+interface State {
+  document: object | null;
+  isPending: boolean;
+  error: string | null;
+  success: boolean;
+}
+
+type Action =
+  | { type: 'SET_IS_PENDING' }
+  | { type: 'ADD_DOC'; payload: object }
+  | { type: 'DELETE_DOC' }
+  | { type: 'EDIT_DOC' }
+  | { type: 'SET_ERROR'; payload: string };
+
+const initialState: State = {
+  document: null,
+  isPending: false,
+  error: null,
+  success: false,
+};
+
+const storeReducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'SET_IS_PENDING':
+      return { ...state, isPending: true, document: null, success: false, error: null };
+    case 'ADD_DOC':
+      return {
+        ...state,
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
+      };
+    case 'DELETE_DOC':
+      return { ...state, isPending: false, document: null, success: true, error: null };
+    case 'EDIT_DOC':
+      return {
+        ...state,
+        isPending: false,
+        success: true,
+        error: null,
+      };
+    case 'SET_ERROR':
+      return {
+        ...state,
+        isPending: false,
+        document: null,
+        success: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const useFirestore = (transaction: string) => {
+  const [response, dispatch] = useReducer(storeReducer, initialState);
+
+  const colRef = collection(appFireStore, transaction);
+
+  const addDocument = async (doc: object) => {
+    dispatch({ type: 'SET_IS_PENDING' });
+    try {
+      const createdTime = timeStamp.fromDate(new Date());
+      const docRef = await addDoc(colRef, { ...doc, createdTime });
+      dispatch({ type: 'ADD_DOC', payload: docRef });
+    } catch (e) {
+      dispatch({ type: 'SET_ERROR', payload: (e as { message: string }).message });
+    }
+  };
+
+  const deleteDocument = async (id: string) => {
+    dispatch({ type: 'SET_IS_PENDING' });
+    try {
+      const docRef = await deleteDoc(doc(colRef, id));
+      dispatch({ type: 'DELETE_DOC' });
+    } catch (e) {
+      dispatch({ type: 'SET_ERROR', payload: (e as { message: string }).message });
+    }
+  };
+
+  const editDocument = async (id: string, inputs: object) => {
+    dispatch({ type: 'SET_IS_PENDING' });
+    try {
+      await updateDoc(doc(colRef, id), inputs);
+      dispatch({ type: 'EDIT_DOC' });
+    } catch (e) {
+      dispatch({ type: 'SET_ERROR', payload: (e as { message: string }).message });
+    }
+  };
+
+  return { addDocument, deleteDocument, editDocument, response };
+};
