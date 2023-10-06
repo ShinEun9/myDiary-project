91ada68531c696684e0709b3045b5ffd7143d647
ShinEun9 <ses2201@naver.com>
2023-10-06 13:05:21 +0900

    feat: useCollection 훅 만듦
    - 컬렉션에 접근하여 데이터를 불러오는 기능 구현
    - useEffect절을 사용하여 컬렉션에 변화가있을때마다 컬렉션을 새로 불러옴

---
+++ src/hooks/useCollection.tsx
@@ -0,0 +1,38 @@
+import { appFireStore } from '../firebase/config';
+import { useEffect, useState } from 'react';
+import { onSnapshot, collection, query, orderBy, QueryConstraint, Query, DocumentData } from 'firebase/firestore';
+import { Document } from '../typings/db';
+
+export const useCollection = (transaction: string, myQuery?: QueryConstraint[]) => {
+  const [documents, setDocuments] = useState<Document[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    let q: Query<DocumentData> = collection(appFireStore, transaction);
+
+    if (myQuery) {
+      q = query(q, ...myQuery, orderBy('createdTime', 'desc'));
+    }
+
+    const unsubscribe = onSnapshot(
+      q,
+      (snapshot) => {
+        const result: Document[] = [];
+        snapshot.docs.forEach((doc) => {
+          result.push({ ...doc.data(), id: doc.id });
+        });
+        setDocuments(result);
+        setError(null);
+      },
+      (error) => {
+        setError(error.message);
+      },
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  }, [collection]);
+
+  return { documents, error };
+};
--- src/typings/db.ts
+++ src/typings/db.ts
@@ -1,3 +1,4 @@
-export interface IUser {
-  displayName: string;
+export interface Document {
+  [key: string]: any;
+  id: string;
 }
