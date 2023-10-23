b4433ee6b2887464c966fade47996df950510f4c
ShinEun9 <ses2201@naver.com>
2023-10-23 17:51:37 +0900

    refactor: 코드 스플리팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
+import { Suspense, lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import Signup from './pages/signup/Signup';
-import NotFound from './pages/notfound/NotFound';
 import useAuthContext from './hooks/useAuthContext';
 import useTitle from './hooks/useTitle';
+
+const Home = lazy(() => import('./pages/home/Home'));
+const Login = lazy(() => import('./pages/login/Login'));
+const Signup = lazy(() => import('./pages/signup/Signup'));
+const NotFound = lazy(() => import('./pages/notfound/NotFound'));
 
 function App() {
   const { isAuthReady, user } = useAuthContext();
   useTitle();
 
   return (
-    <div>
-      {isAuthReady ? (
-        <Routes>
-          <Route path="/" element={user ? <Home /> : <Navigate replace={true} to="/login" />}></Route>
-          <Route path="/login" element={!user ? <Login /> : <Navigate to="/" replace={true} />}></Route>
-          <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" replace={true} />}></Route>
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      ) : (
-        <div className="loading">loading...</div>
-      )}
-    </div>
+    <Suspense fallback={<div className="loading">loading...</div>}>
+      <div>
+        {isAuthReady ? (
+          <Routes>
+            <Route path="/" element={user ? <Home /> : <Navigate replace={true} to="/login" />}></Route>
+            <Route path="/login" element={!user ? <Login /> : <Navigate to="/" replace={true} />}></Route>
+            <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" replace={true} />}></Route>
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        ) : (
+          <div className="loading">loading...</div>
+        )}
+      </div>
+    </Suspense>
   );
 }
 
